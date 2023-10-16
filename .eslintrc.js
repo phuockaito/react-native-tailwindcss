fd391b8fb1526ac73df4014013c30a0d25e7f95d
Phuoc <huuphuocit1999@gmail.com>
2023-10-16 15:13:34 +0700

    fix: className Error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,17 @@
 module.exports = {
     root: true,
-    // extends: "@react-native",
-    extends: '@react-native-community',
-    rules: {
-        quotes: [1, 'single'],
-    }
+    extends: ['@react-native-community'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    overrides: [{
+        files: ['*.ts', '*.tsx'],
+        rules: {
+            '@typescript-eslint/no-shadow': ['error'],
+            'no-shadow': 'off',
+            'no-undef': 'off'
+        }
+    }],
+    "eslint.workingDirectories": [
+        "./dir/containing/eslintrc_file",
+    ]
 };
--- App.tsx
+++ App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { SafeAreaView, Text, View } from 'react-native';
 
-
 const App = () => {
     return (
         <SafeAreaView>
             <View>
-                <Text className="text-red-500">Hello word</Text>
+                <Text className="text-red-500">Hello word </Text>
             </View>
         </SafeAreaView>
-    )
+    );
 };
 export default App;
